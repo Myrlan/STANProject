00ac2c9b027f04f9a2716445c5dc21396cd42d03
TristanM42 <37584943+TristanM42@users.noreply.github.com>
2019-11-19 15:41:33 +0100

    Première version de connexion
    - lancement du navigateur
    - affichage du html au démarrage (le get "/start")
    - récupération des informations de connexion dans le post "/login"

---
+++ routes/game_routes.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/mydb')
+
+// chargement du modèle User
+const User = require("../models/user.model.js")
+
+var path = require('path');
+const router = express.Router()
+
+router.get('/start', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main_menu.html'));
+})
+
+router.post('/register', (req, res) => {
+    
+})
+
+router.post('/login', (req, res) => {
+  console.log("login called")
+  const { username, password } = req.body
+  console.log(req.session)
+  if(username === 'sessId'){
+    res.send(req.body.username + " existe")
+  }else{
+    res.send(req.body.username + " n'existe pas")
+  }
+})
+
+module.exports = router
