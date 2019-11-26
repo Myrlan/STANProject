09041a5acb759210430c1c7c3ff79aa4a70e821e
TristanM42 <37584943+TristanM42@users.noreply.github.com>
2019-11-26 17:52:15 +0100

    Login et register implémenté
    login ne marche pas encore si l'utilisateur n'existe pas ! (try catch ne marche pas et function(err,result) non plus)

--- routes/game_routes.js
+++ routes/game_routes.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/mydb')
+// Connexion à MongoDB :
+var mongo = require('mongodb')
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/gameDB";
+//const mongoose = require('mongoose')
+//mongoose.connect('mongodb://localhost/mydb')
 
 // chargement du modèle User
 const User = require("../models/user.model.js")
@@ -14,6 +18,24 @@ router.get('/start', (req, res) => {
 
 router.post('/register', (req, res) => {
   console.log("truc machin register")
+  const { username, password } = req.body
+  MongoClient.connect(url,
+    function(err, db) {
+    if (err) throw err;
+    console.log("Database connected!");
+    var dbo = db.db("gameDB");
+  
+    dbo.collection("players").insertOne({"name":username,"password":password}, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+    });
+
+    var results = dbo.collection("players").find({name:username});
+    results.forEach(row => {
+        console.log(row);
+    });
+  });
   res.sendFile(path.join(__dirname + '/connecte.html'))
 })
 
@@ -21,11 +43,29 @@ router.post('/login', (req, res) => {
   console.log("login called")
   const { username, password } = req.body
   console.log(req.session)
-  if(username === 'sessId'){
-    res.send(req.body.username + " existe")
-  }else{
-    res.send(req.body.username + " n'existe pas")
-  }
+  // Test d'existence :
+  MongoClient.connect(url,
+    function(err, db) {
+    if (err) throw err;
+    console.log("Database connected!");
+    var dbo = db.db("gameDB");
+
+    var results = dbo.collection("players").find({name:username});
+    try{  // ne marche pas, avec function err,result non plus :'(
+      results.forEach(row => {
+        console.log(row.name);
+        //if(username === row.name){
+          res.sendFile(path.join(__dirname + '/connecte.html'))
+          //res.send(req.body.username + " existe")
+      });
+    } 
+    catch(err){ 
+      // res.sendFile(path.join(__dirname + '/Mal_connecte.html'))
+      // res.send("Echec")
+      console.log("Echec log")
+      // res.send(req.body.username + " n'existe pas")
+    }
+  });
 })
 
 module.exports = router
