45052990e567655a1521cbb958154de57484062c
tita2 <57151485+tita2@users.noreply.github.com>
2019-11-26 15:22:08 +0100

    html et lien avec game_routes

--- routes/game_routes.js
+++ routes/game_routes.js
@@ -1,29 +1,27 @@
 const express = require('express')
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/mydb')
 
 // chargement du modèle User
-const User = require("../models/user.model.js")
+const User = require('../models/user.model.js')
 
-var path = require('path');
+var path = require('path')
 const router = express.Router()
 
 router.get('/start', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main_menu.html'));
+  res.sendFile(path.join(__dirname + '/main_menu.html'))
 })
 
 router.post('/register', (req, res) => {
-    
+
 })
 
 router.post('/login', (req, res) => {
-  console.log("login called")
+  console.log('login called')
   const { username, password } = req.body
   console.log(req.session)
-  if(username === 'sessId'){
-    res.send(req.body.username + " existe")
-  }else{
-    res.send(req.body.username + " n'existe pas")
+  if (username === 'sessId') {
+    res.sendFile(path.join(__dirname + '/connecte.html'))
+  } else {
+    res.sendFile(path.join(__dirname + '/Mal_connecte.html'))
   }
 })
 
