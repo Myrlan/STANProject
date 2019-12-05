633de1e84e13f5720044b7cb85a4b061da774669
TristanM42 <37584943+TristanM42@users.noreply.github.com>
2019-12-05 17:42:24 +0100

    Test ejs pour affichage dynamique + ajout des boutons play et settings sur la page connecte.html

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ MongoClient.connect(url,
 });
 
 const app = express()
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false })) // for simple form posts
 app.use(bodyParser.json()) // for API requests
 
--- routes/game_routes.js
+++ routes/game_routes.js
@@ -105,4 +105,18 @@ router.post('/login', (req, res) => {
   });
 })
 
+router.get('/play', (req, res) => {
+  //res.render('game_UI')//, {page:'Home', menuId:'home'});
+  var drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 }
+];
+var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+  res.render('game_UI', {drinks : drinks, tagline : tagline})
+  /* Ligne à mettre dans .ejs :
+    <img src="<%= document.data.illustration.url %>" />
+    */
+})
+
 module.exports = router
